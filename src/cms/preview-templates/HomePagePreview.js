25b68d8edc05c69c052593ed86731264ba694fbb
edward01s2 <edward.01s2@gmail.com>
2019-01-17 16:54:17 -0600

    Trying to fix netlify

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -5,7 +5,6 @@ import { HomePageTemplate } from '../../templates/home-page'
 const HomePagePreview = ({ entry, widgetFor }) => (
   <HomePageTemplate
     title={entry.getIn(['data', 'hero'])}
-    content={widgetFor('body')}
   />
 )
 
