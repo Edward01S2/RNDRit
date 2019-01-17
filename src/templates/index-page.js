f9f75bbabab519c6e8bd55ec098d9285c5dc9c1b
edward01s2 <edward.01s2@gmail.com>
2019-01-16 21:53:10 -0600

    Added index page for cms

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,7 +4,7 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 
-export const HomePageTemplate = ({ title, content, contentComponent }) => {
+export const IndexPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
@@ -25,18 +25,18 @@ export const HomePageTemplate = ({ title, content, contentComponent }) => {
   )
 }
 
-HomePageTemplate.propTypes = {
+IndexPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
 }
 
-const HomePage = ({ data }) => {
+const IndexPage = ({ data }) => {
   const { markdownRemark: post } = data
 
   return (
     <Layout>
-      <HomePageTemplate
+      <IndexPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
@@ -45,14 +45,14 @@ const HomePage = ({ data }) => {
   )
 }
 
-HomePage.propTypes = {
+IndexPage.propTypes = {
   data: PropTypes.object.isRequired,
 }
 
-export default HomePage
+export default IndexPage
 
-export const homePageQuery = graphql`
-  query HomePage($id: String!) {
+export const indexPageQuery = graphql`
+  query IndexPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
