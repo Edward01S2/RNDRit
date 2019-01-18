3b9a37bc44ce2a1db74a991794fe5594742d3581
edward01s2 <edward.01s2@gmail.com>
2019-01-17 22:34:32 -0600

    This time

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -1,55 +1,40 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+//import PropTypes from 'prop-types'
+// import { graphql } from 'gatsby'
+// import Layout from '../components/Layout'
+// import Content, { HTMLContent } from '../components/Content'
 
-export const HomePageTemplate = ({ hero, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+export const HomePageTemplate = () => {
 
   return (
     <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {hero}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
-      </div>
+      <div>Here</div>
     </section>
   )
 }
 
-HomePageTemplate.propTypes = {
-  hero: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
-}
+// HomePageTemplate.propTypes = {
+//   //hero: PropTypes.string.isRequired,
+//   //content: PropTypes.string,
+//   //contentComponent: PropTypes.func,
+// }
 
-const HomePage = ({ data }) => {
-  const { markdownRemark: post } = data
+// const HomePage = ({ data }) => {
+//   const { markdownRemark: post } = data
 
-  return (
-    <Layout>
-      <HomePageTemplate
-        contentComponent={HTMLContent}
-        hero={post.frontmatter.hero}
-        content={post.html}
-      />
-    </Layout>
-  )
-}
+//   return (
+//     <Layout>
+//       <HomePageTemplate
+//       />
+//     </Layout>
+//   )
+// }
 
-HomePage.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+// HomePage.propTypes = {
+//   data: PropTypes.object.isRequired,
+// }
 
-export default HomePage
+//export default HomePage
 
 // export const homePageQuery = graphql`
 //   query HomePage($id: String!) {
