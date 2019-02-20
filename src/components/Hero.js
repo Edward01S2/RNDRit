ca99849ad4627beada31a399b6708527a00e1ca4
edward01s2 <edward.01s2@gmail.com>
2019-02-20 10:22:13 -0600

    YML for service page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,17 +2,25 @@ import React from "react";
 
 const Hero = class extends React.Component {
   render() {
+    function createMarkup(cms) {
+      return { __html: cms };
+    }
     return (
       <section id="hero" className="container mx-auto">
         <div className="py-40 px-4 xl:py-56">
           <div className="font-robot md:text-center md:w-3/5 md:m-auto lg:w-1/2">
-            <h1 className="font-bold text-5xl xl:text-6xl">{this.props.heading}</h1>
-            <p className="pt-4 text-xl leading-normal xl:text-2xl">{this.props.description}</p>
+            <h1
+              className="font-bold text-5xl xl:text-6xl"
+              dangerouslySetInnerHTML={createMarkup(this.props.heading)}
+            />
+            <p className={`pt-4 text-xl leading-normal xl:text-2xl ${this.props.pClass}`}>
+              {this.props.description}
+            </p>
           </div>
         </div>
       </section>
-    )
+    );
   }
-}
+};
 
 export default Hero;
--- src/templates/service-page.js
+++ src/templates/service-page.js
@@ -1,68 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Content, { HTMLContent } from '../components/Content'
-
-export const ServicePageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
-
-  return (
-    // <section className="section section--gradient">
-    //   <div className="container">
-    //     <div className="columns">
-    //       <div className="column is-10 is-offset-1">
-    //         <div className="section">
-    //           <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-    //             {title}
-    //           </h2>
-    //           <PageContent className="content" content={content} />
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
-    <section className="bg-blue-darkest text-white">
-
-      <Hero />
-      
-      <div className="container mx-auto">
-        <PageContent className="content" content={content} />
-      </div>
-    </section>
-  )
-}
-
-ServicePageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
-}
+import Content, { HTMLContent } from "../components/Content";
 
 const ServicePage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
+  const PageContent = HTMLContent || Content;
 
   return (
     <Layout>
-      <Navbar classes="bg-blue-darkest" logo="text-white lg:text-5xl" navItems="text-grey-dark" menuBg="bg-blue-darker" menuBtn="text-white"/>
-      <ServicePageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
+      <Navbar
+        classes="bg-blue-darkest"
+        logo="text-white lg:text-5xl"
+        navItems="text-grey-dark"
+        menuBg="bg-blue-darker"
+        menuBtn="text-white"
       />
+      <section className="bg-blue-darkest text-white">
+        <Hero
+          heading={post.frontmatter.hero.heading}
+          description={post.frontmatter.hero.description}
+          pClass="text-grey-dark"
+        />
+
+        <div className="container mx-auto">
+          <PageContent className="content" content={post.html} />
+        </div>
+      </section>
+
       <Footer />
     </Layout>
-  )
-}
+  );
+};
 
 ServicePage.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+  data: PropTypes.object.isRequired
+};
 
-export default ServicePage
+export default ServicePage;
 
 export const servicePageQuery = graphql`
   query ServicePage($id: String!) {
@@ -70,7 +49,11 @@ export const servicePageQuery = graphql`
       html
       frontmatter {
         title
+        hero {
+          heading
+          description
+        }
       }
     }
   }
-`
+`;
