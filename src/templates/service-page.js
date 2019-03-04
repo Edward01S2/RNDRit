4f2d58ac2428b98adeb534b7e88fdb8930841b92
edward01s2 <edward.01s2@gmail.com>
2019-03-04 11:13:24 -0600

    Add cms fields for service page

--- src/templates/service-page.js
+++ src/templates/service-page.js
@@ -8,7 +8,7 @@ import Footer from "../components/Footer";
 import Content, { HTMLContent } from "../components/Content";
 import uuid from "uuid";
 import TriSvg from "../img/triangle.svg";
-import CirSvg from "../img/circle.svg";
+import WebSvg from "../img/web.svg";
 import ScrollAnimation from "react-animate-on-scroll";
 
 const ServicePage = ({ data }) => {
@@ -35,48 +35,12 @@ const ServicePage = ({ data }) => {
         <section className="container mx-auto">
           <div className="flex flex-col px-8">
             <div className="mx-auto mb-12">
-              <ScrollAnimation animateIn="tri-top tri-mid tri-bot" animatePreScroll={false} animateOnce={true}>
+              <ScrollAnimation
+                animateIn="tri-top tri-mid tri-bot"
+                animatePreScroll={false}
+                animateOnce={true}
+              >
                 <TriSvg className="triangle stroke-current text-grey-lightest h-64 w-64" />
-              </ScrollAnimation>
-            </div>
-            <div>
-              <h3 className="text-2xl pb-4 leading-normal">
-                {post.frontmatter.point.heading}
-              </h3>
-              <div className="text-grey-dark leading-loose">
-                <p className="pb-4">{post.frontmatter.point.body}</p>
-                <ul className="list-reset">
-                  {post.frontmatter.point.bullets.map(tag => (
-                    <li key={uuid.v4()} className="flex items-center">
-                      <svg
-                        className="h-4 w-4 fill-current"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="1" 
-                        height="1"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM6.7 9.29L9 11.6l4.3-4.3 1.4 1.42L9 14.4l-3.7-3.7 1.4-1.42z" />
-                      </svg>
-                      <span className="pl-2">{tag}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section>
-          <div className="px-8 py-24 text-grey-dark text-center">
-            <h3 className="text-3xl">{post.frontmatter.header}</h3>
-          </div>
-        </section>
-
-        <section className="container mx-auto">
-          <div className="flex flex-col px-8">
-            <div className="mx-auto mb-12">
-              <ScrollAnimation animateIn='grp1 grp2 grp3 grp4' animatePreScroll={false} animateOnce={true}>
-                <CirSvg className="circle stroke-current text-white h-64 w-64" />
               </ScrollAnimation>
             </div>
             <div>
@@ -105,6 +69,53 @@ const ServicePage = ({ data }) => {
             </div>
           </div>
         </section>
+
+        <section>
+          <div className="px-8 py-24 text-grey-dark text-center">
+            <h3 className="text-3xl">{post.frontmatter.header}</h3>
+          </div>
+        </section>
+
+        <section className="container mx-auto pb-24">
+          <div className="flex flex-col px-8">
+            <div className="mx-auto mb-12">
+              <ScrollAnimation
+                animateIn="grp1 grp2 grp3 grp4"
+                animatePreScroll={false}
+                animateOnce={true}
+              >
+                <WebSvg className="circle stroke-current fill-current text-white h-64 w-64" />
+              </ScrollAnimation>
+            </div>
+            <div>
+              <h3 className="text-2xl pb-4 leading-normal">
+                {post.frontmatter.point.heading}
+              </h3>
+              <div className="text-grey-dark leading-loose">
+                <p className="pb-4">{post.frontmatter.point.body}</p>
+                <ul className="list-reset">
+                  {post.frontmatter.point.bullets.map(tag => (
+                    <li key={uuid.v4()} className="flex items-center">
+                      <svg
+                        className="h-4 w-4 fill-current"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="1"
+                        height="1"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM6.7 9.29L9 11.6l4.3-4.3 1.4 1.42L9 14.4l-3.7-3.7 1.4-1.42z" />
+                      </svg>
+                      <span className="pl-2">{tag}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* <Tech data={it} width={36} /> */}
+
       </div>
 
       <Footer />
